5d938307786e406fa7bdb025c051e5983987fea3
Alexandre <ale.developer72@gmail.com>
2025-05-08 19:11:09 -0300

    Listagem de produtos parte 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from "./components/NavBar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -17,7 +18,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={inter.className}>
-        {children}
+          <NavBar />
+        <main className="bg-slate-700 h-screen p-16">{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,22 @@
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold bg-gray-500 underline">oi</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg overflow-hidden">
+          Prod 1
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden">
+          Prod 2
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden">
+          Prod 3
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden">
+          Prod 4
+        </div>
+
+      </div>
+    </div>
   );
 }
