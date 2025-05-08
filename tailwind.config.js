ea39628ec90202482fb5dd230c256fb17bdb3ad3
Alexandre <ale.developer72@gmail.com>
2025-05-07 21:41:02 -0300

    Codigo Inicial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,ts,jsx,tsx}", // ou o caminho do seu código
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
